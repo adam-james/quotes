4650c0e60441472fc00808b0a0694e04e0553b8e
adam-james <adam.j.sulewski@gmail.com>
2018-04-12 23:52:26 -0600

    style author page

--- src/pages/AuthorDetailPage.js
+++ src/pages/AuthorDetailPage.js
@@ -2,36 +2,40 @@ import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { Mutation, Query } from 'react-apollo'
 import gql from 'graphql-tag'
+import styled from 'styled-components'
+import ListSection from '../containers/ListSection'
+import { SectionTitle } from '../components/Layout'
+import { Main } from '../components/Layout'
 
-class CreateQuoteForm extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { body: '' }
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-  }
+// class CreateQuoteForm extends React.Component {
+//   constructor (props) {
+//     super(props)
+//     this.state = { body: '' }
+//     this.handleSubmit = this.handleSubmit.bind(this)
+//     this.handleChange = this.handleChange.bind(this)
+//   }
 
-  handleSubmit (e) {
-    e.preventDefault()
-    this.props.onSubmit({ body: this.state.body })
-    this.setState({ body: '' })
-  }
+//   handleSubmit (e) {
+//     e.preventDefault()
+//     this.props.onSubmit({ body: this.state.body })
+//     this.setState({ body: '' })
+//   }
 
-  handleChange (e) {
-    const { value } = e.target
-    this.setState({ body: value })
-  }
+//   handleChange (e) {
+//     const { value } = e.target
+//     this.setState({ body: value })
+//   }
 
-  render () {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <h3>Add Quote</h3>
-        <input onChange={this.handleChange} value={this.state.body} />
-        <button type='submit'>Add Quote</button>
-      </form>
-    )
-  }
-}
+//   render () {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <h3>Add Quote</h3>
+//         <input onChange={this.handleChange} value={this.state.body} />
+//         <button type='submit'>Add Quote</button>
+//       </form>
+//     )
+//   }
+// }
 
 const GET_AUTHOR = gql`
   query Author($id: ID!) {
@@ -46,89 +50,78 @@ const GET_AUTHOR = gql`
   }
 `
 
-function AuthorDetailPage ({ match }) {
+// const CREATE_QUOTE = gql`
+//   mutation createQuote($authorId: ID!, $body: String!) {
+//     createQuote(authorId: $authorId, body: $body) {
+//       id
+//       body
+//     }
+//   }
+// `
+
+// function CreateQuote ({ authorId }) {
+//   const handleSubmit = (createQuote) => ({ body }) => {
+//     createQuote({ variables: { authorId, body } })
+//   }
+
+//   return (
+//     <Mutation
+//       mutation={CREATE_QUOTE}
+//       update={(cache, { data: { createQuote } }) => {
+//         const { Author } = cache.readQuery({
+//           query: GET_AUTHOR,
+//           variables: { id: authorId }
+//         })
+//         const data = {
+//           Author: {
+//             ...Author,
+//             quotes: Author.quotes.concat([ createQuote ])
+//           }
+//         }
+//         cache.writeQuery({
+//           query: GET_AUTHOR,
+//           data
+//         })
+//       }}
+//     >
+//       {(createQuote) => (
+//         <CreateQuoteForm onSubmit={handleSubmit(createQuote)} />
+//       )}
+//     </Mutation>
+//   )
+// }
+
+const QuoteBody = styled.p`
+  margin: 0;
+  text-align: center;
+  font-family: sans-serif;
+  font-size: 16px;
+  line-height: 1.5em;  
+`
+
+const Author = ({ name, quotes }) => (
+  <ListSection title={`Quotes from ${name}`} items={quotes}>
+    {({ body }) => <QuoteBody>{body}</QuoteBody>}
+  </ListSection>
+)
+
+const render = ({ loading, error, data }) => {
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
+  return (
+    <Main>
+      <Author {...data.Author} />
+    </Main>
+  )
+}
+
+const AuthorDetailPage = ({ match }) => {
   const { id } = match.params
   return (
     <Query query={GET_AUTHOR} variables={{ id }}>
-      {({ loading, error, data }) => {
-        if (loading) return <p>Loading...</p>
-        if (error) return <p>Error :(</p>
-
-        return (
-          <main>
-            <CreateQuote authorId={data.Author.id} />
-            <ShowAuthor author={data.Author} />
-          </main>
-        )
-      }}
+      {render}
     </Query>
-  )
-}
-
-function ShowAuthor ({ author }) {
-  return (
-    <section>
-      <h2>{author.name}</h2>
-      <QuoteList quotes={author.quotes} />
-    </section>
-  )
-}
-
-function QuoteList ({ quotes }) {
-  return (
-    <section>
-      <h3>Quotes</h3>
-      <ul>
-        {
-          quotes.map(quote => (
-            <li key={quote.id}>
-              <em>{quote.body}</em>
-            </li>
-          ))
-        }
-      </ul>
-    </section>
-  )
-}
-
-const CREATE_QUOTE = gql`
-  mutation createQuote($authorId: ID!, $body: String!) {
-    createQuote(authorId: $authorId, body: $body) {
-      id
-      body
-    }
-  }
-`
-
-function CreateQuote ({ authorId }) {
-  const handleSubmit = (createQuote) => ({ body }) => {
-    createQuote({ variables: { authorId, body } })
-  }
-
-  return (
-    <Mutation
-      mutation={CREATE_QUOTE}
-      update={(cache, { data: { createQuote } }) => {
-        const { Author } = cache.readQuery({
-          query: GET_AUTHOR,
-          variables: { id: authorId }
-        })
-        const data = {
-          Author: {
-            ...Author,
-            quotes: Author.quotes.concat([ createQuote ])
-          }
-        }
-        cache.writeQuery({
-          query: GET_AUTHOR,
-          data
-        })
-      }}
-    >
-      {(createQuote) => (
-        <CreateQuoteForm onSubmit={handleSubmit(createQuote)} />
-      )}
-    </Mutation>
   )
 }
 
