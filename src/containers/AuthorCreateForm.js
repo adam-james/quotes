56accd3ed263fc013e2ea503938e79c048c264ba
adam-james <adam.j.sulewski@gmail.com>
2018-04-15 14:53:25 -0600

    test author create form

--- src/containers/AuthorCreateForm.js
+++ src/containers/AuthorCreateForm.js
@@ -2,20 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Form, Text } from 'react-form'
 
+export const render = (formApi) => (
+  <form onSubmit={formApi.submitForm}>
+    <label htmlFor='firstName'>First Name</label>
+    <Text field='firstName' id='firstName' />
+
+    <label htmlFor='lastName'>Last Name</label>
+    <Text field='lastName' id='lastName' />
+
+    <button type='submit'>Submit</button>
+  </form>
+)
+
 const AuthorCreateForm = ({ onSubmit }) => (
   <Form onSubmit={onSubmit}>
-    {formApi => (
-      <form onSubmit={formApi.submitForm}>
-
-        <label htmlFor='firstName'>First Name</label>
-        <Text field='firstName' id='firstName' />
-
-        <label htmlFor='lastName'>Last Name</label>
-        <Text field='lastName' id='lastName' />
-
-        <button type='submit'>Submit</button>
-      </form>
-    )}
+    {render}
   </Form>
 )
 
