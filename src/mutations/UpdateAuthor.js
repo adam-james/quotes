af2f51a6afc0428183a1011172b34a62770ba220
adam-james <adam.j.sulewski@gmail.com>
2018-04-17 22:48:03 -0600

    add update author

---
+++ src/mutations/UpdateAuthor.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+import { GET_AUTHOR } from '../queries/GetAuthor'
+
+export const UPDATE_AUTHOR = gql`
+  mutation updateAuthor($id: ID!, $firstName: String, $lastName: String) {
+    updateAuthor(id: $id, firstName: $firstName, lastName: $lastName) {
+      id
+      firstName
+      lastName
+    }
+  }
+`
+
+class UpdateAuthor extends React.Component {
+  constructor (props) {
+    super(props)
+    this.update = this.update.bind(this)
+  }
+
+  update (cache, { data: { updateAuthor } }) {
+    const { author } = cache.readQuery({
+      query: GET_AUTHOR,
+      variables: { id: this.props.id }
+    })
+    cache.writeQuery({
+      query: GET_AUTHOR,
+      data: {
+        author: {
+          quotes: author.quotes,
+          ...updateAuthor
+        }
+      }
+    })
+  }
+
+  render () {
+    return (
+      <Mutation mutation={UPDATE_AUTHOR} update={this.update} >
+        {updateAuthor => this.props.children(updateAuthor)}
+      </Mutation>
+    )
+  }
+}
+
+UpdateAuthor.propTypes = {
+  id: PropTypes.string.isRequired,
+  children: PropTypes.func.isRequired
+}
+
+export default UpdateAuthor
--- src/views/AuthorEditView.js
+++ src/views/AuthorEditView.js
@@ -2,19 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withRouter } from 'react-router-dom'
 import GetAuthor from '../queries/GetAuthor'
+import UpdateAuthor from '../mutations/UpdateAuthor'
 import { Main } from '../components/Layout'
 import { Card, CardTitle } from '../components/card'
 import AuthorForm from '../containers/AuthorForm'
 
-export const AuthorEditView = ({ author }) => {
-  return (
-    <Main>
-      <Card>
-        <CardTitle>Edit Author</CardTitle>
-        <AuthorForm {...author} onSubmit={(result) => {console.log(result)}} />
-      </Card>
-    </Main>
-  )
+export class AuthorEditView extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit ({ firstName, lastName }) {
+    const { id } = this.props.author
+    this.props.updateAuthor({ variables: { id, firstName, lastName }})
+    this.props.history.push(`/authors/${id}`)
+  }
+
+  render () {    
+    return (
+      <Main>
+        <Card>
+          <CardTitle>Edit Author</CardTitle>
+          <AuthorForm {...this.props.author} onSubmit={this.handleSubmit} />
+        </Card>
+      </Main>
+    )
+  }
 }
 
 AuthorEditView.propTypes = {
@@ -22,17 +36,26 @@ AuthorEditView.propTypes = {
     firstName: PropTypes.string.isRequired,
     lastName: PropTypes.string.isRequired,
     id: PropTypes.string.isRequired,
-    quotes: PropTypes.array.isRequired
   }).isRequired,
   updateAuthor: PropTypes.func.isRequired
 }
 
-const Wrapper = ({ match }) => {
+const Wrapper = ({ match, history }) => {
   const { id } = match.params
 
   return (
     <GetAuthor id={id}>
-      {author => <AuthorEditView author={author} />}
+      {author => (
+        <UpdateAuthor id={author.id}>
+          {updateAuthor => (
+            <AuthorEditView
+              author={author}
+              updateAuthor={updateAuthor}
+              history={history}
+            />
+          )}
+        </UpdateAuthor>
+      )}
     </GetAuthor>
   )
 }
