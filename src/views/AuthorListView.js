150843a8dc1c787d2d4d4aaa9767116d61c36d21
adam-james <adam.j.sulewski@gmail.com>
2018-04-18 14:49:26 -0600

    flow from index to create quote

--- src/views/AuthorListView.js
+++ src/views/AuthorListView.js
@@ -2,9 +2,15 @@ import React from 'react'
 import { Main } from '../components/Layout'
 import { LinkButton } from '../components/Button'
 import AuthorSearch from '../containers/AuthorSearch'
+import { Card, CardTitle } from '../components/card'
 
 const AuthorListView = () => (
   <Main>
+    <Card>
+      <CardTitle style={{ margin: '0' }}>
+        To add a quote, first find or create an author.
+      </CardTitle>
+    </Card>
     <LinkButton to='add-author'>+ Add Author</LinkButton>
     <AuthorSearch />
   </Main>
--- src/views/QuoteListView.js
+++ src/views/QuoteListView.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Main, SectionTitle } from '../components/Layout'
 import QuoteList from '../containers/QuoteList'
+import { LinkButton } from '../components/Button'
 
 const QuoteListView = () => (
   <Main>
+    <LinkButton to='/authors'>
+      + Add Quote
+    </LinkButton>
     <section>
       <SectionTitle>Recently Added Quotes</SectionTitle>
       <QuoteList />
